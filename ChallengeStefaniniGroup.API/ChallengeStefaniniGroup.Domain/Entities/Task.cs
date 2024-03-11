2ec2c68c36db41ddeb5ec13ed38122005b493a9f
Robson Liesner de Lima Junior <robsonlljr@gmail.com>
2024-03-11 19:27:07 -0300

    Criado classe Task e enum TaskStatusEnum

---
+++ ChallengeStefaniniGroup.API/ChallengeStefaniniGroup.Domain/Entities/Task.cs
@@ -0,0 +1,12 @@
+﻿using ChallengeStefaniniGroup.Domain.Enum;
+
+namespace ChallengeStefaniniGroup.Domain.Entities
+{
+    public class Task
+    {
+        public string Title { get; set; } = string.Empty!;
+        public string Description { get; set; } = string.Empty!;
+        public DateTime DateCreated { get; set; }
+        public TaskStatusEnum Status { get; set; }
+    }
+}
---
+++ ChallengeStefaniniGroup.API/ChallengeStefaniniGroup.Domain/Enum/TaskStatusEnum.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChallengeStefaniniGroup.Domain.Enum
+{
+    public enum TaskStatusEnum
+    {
+        [Display(Name = "Pendente")]
+        Pending = 0,
+        [Display(Name = "Em Andamento")]
+        Progress = 1,
+        [Display(Name = "Concluída")]
+        Completed = 2
+    }
+}
