b6f8ffda832e774082b788544a7fd3aba034df2f
Robson Liesner de Lima Junior <robsonlljr@gmail.com>
2024-03-12 17:34:20 -0300

    Importado bibliotecas axios, vue-axios, bootstrap-vue, bootstrap, popperjs/core e sweetalert2

--- ChallengeStefaniniGroup.UI/src/main.js
+++ ChallengeStefaniniGroup.UI/src/main.js
@@ -3,9 +3,14 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const app = createApp(App)
 
 app.use(router)
+app.use(VueAxios, axios);
 
 app.mount('#app')
